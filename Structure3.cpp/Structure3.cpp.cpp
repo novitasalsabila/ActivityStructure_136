b08e8dcd8e5fa8d55858afd70d019f57377f814f
novitasalsabila <novnovv10@gmail.com>
2023-04-03 11:28:58 +0700

    Membuat structure menggunakan array dan looping atau perulangan for

---
+++ Structure3.cpp/Structure3.cpp.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+struct AlamatDetail
+{
+	char desa;
+	char kota;
+};
+struct Mahasiswa
+{
+	char NIM[12];
+	char nama[25];
+	AlamatDetail alamat;
+	int umur;
+};
+int main() {
+	Mahasiswa mhs[3]
+
+	for (int i = 0;i < 3;i++) {
+		cout << "Masukan NIM :";
+		cin.getline(mhs[i] NIM, 12);
+		cout << "Masukan Nama :";
+		cin.getline(mhs[i] nama, 25);
+		cout << "Alamat :";
+		cout << "\n\tMasukan Desa :";
+		cin.getline(mhs[i].alamat.desa, 20);
+		cout << "\n\tMasukan Kota :";
+		cin.getline(mhs[i].alamat.kota, 20);
+		cout << "Masukan Umur :";
+		cin >> mhs[i] umur;
+		cin >> ignore(1, '\n');
+	}
+
+	
+	for (int i = 0;i < 3;i++)
+	{
+		cout << "\n\nNIM =" << mhs[i].NIM;
+		cout << "\nNama =" << mhs[i].nama;
+		cout << "\nDesa =" << mhs[i].alamat.desa;
+		cout << "\nKota =" << mhs[i].alamat.kota;
+		cout << "\nUmur =" << mhs[i].umur;
+	}
+
+		
+}
