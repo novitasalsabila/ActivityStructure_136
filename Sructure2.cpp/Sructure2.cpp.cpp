883db1bdf7e176b1deebc6b201b867fd89b4bda9
novitasalsabila <novnovv10@gmail.com>
2023-04-03 10:49:08 +0700

    Membuat structure dalam structure

---
+++ Sructure2.cpp/Sructure2.cpp.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+
+struct AlamatDetail
+{
+	string desa;
+	string kota;
+};
+
+struct Mahasiswa
+{
+	string NIM;
+	string nama;
+	AlamatDetail alamat;
+	int umur;
+};
