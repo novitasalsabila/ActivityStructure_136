ed094cb80ff32204c27de2031537639beea418ec
novitasalsabila <novnovv10@gmail.com>
2023-04-03 11:04:53 +0700

    Menampilkan hasil output dengan menggunakan prosedure main

--- Sructure2.cpp/Sructure2.cpp.cpp
+++ Sructure2.cpp/Sructure2.cpp.cpp
@@ -15,3 +15,24 @@ struct Mahasiswa
 	AlamatDetail alamat;
 	int umur;
 };
+
+int main() {
+	Mahasiswa mhs;
+
+	cout << "Masukan NIM :";
+	cin >> mhs.NIM;
+	cout << "Masukan Nama :";
+	cin >> mhs.nama;
+	cout << "Alamat :";
+	cout << "\n\tMasukan Desa :";
+	cin >> mhs.alamat.desa;
+	cout << "\tMasukan Kota :";
+	cout << "Masukan Umur :";
+	cin >> mhs.umur;
+
+	cout << "\nNIM =" << mhs.NIM;
+	cout << "\nNama =" << mhs.nama;
+	cout << "\nDesa =" << mhs.alamat.desa;
+	cout << "\nKota =" << mhs.alamat.kota;
+	cout << "\nUmur =" << mhs.umur;
+}
