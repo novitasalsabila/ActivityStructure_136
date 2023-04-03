885f284956e4cc43039d33b264ebee11bfa3f4c7
novitasalsabila <novnovv10@gmail.com>
2023-04-03 10:30:17 +0700

    Menggunakan prosedure main untuk mengeksekusi program

--- ActivityStructure_136/ActivityStructure_136.cpp
+++ ActivityStructure_136/ActivityStructure_136.cpp
@@ -8,5 +8,23 @@ struct mahasiswa
 	string alamat;
 	int umur;
 };
+int main() {
+	mahasiswa mhs1, mhs2;
 
+	mhs1.NIM = "20220140136";
+	mhs1.nama = "Novita Salsabila";
+	mhs1.alamat = " yogyakarta";
+	mhs1.umur = 18;
+
+	cout << "Masukan NIM :";
+	cin >> mhs2.NIM;
+	cout << "Masukan Nama :";
+	cin >> mhs2.nama;
+	cout << "Masukan Alamat :";
+	cin >> mhs2.alamat;
+	cout << "Masukan Umur :";
+	cin >> mhs2.umur;
+
+
+}
 
