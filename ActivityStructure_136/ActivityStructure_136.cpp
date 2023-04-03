685ed2ff9bb30ae8f62cc991a5fa76e79dba5f49
novitasalsabila <novnovv10@gmail.com>
2023-04-03 10:35:01 +0700

    Menampilkan output dari struct mahasiswa

--- ActivityStructure_136/ActivityStructure_136.cpp
+++ ActivityStructure_136/ActivityStructure_136.cpp
@@ -25,6 +25,15 @@ int main() {
 	cout << "Masukan Umur :";
 	cin >> mhs2.umur;
 
+	cout << "\nNIM =" << mhs1.NIM;
+	cout << "\nNama =" << mhs1.nama;
+	cout << "\nAlamat =" << mhs1.alamat;
+	cout << "\nUmur =" << mhs1.umur;
+
+	cout << "\nNIM =" << mhs2.NIM;
+	cout << "\nNama =" << mhs2.nama;
+	cout << "\nAlamat =" << mhs2.alamat;
+	cout << "\nUmur =" << mhs2.umur;
 
 }
 
